073f1fb3f5eee73da19be74b275cc75887f69aa9
rendi <rendimaulana953@gmail.com>
2022-08-01 14:20:37 +0700

    hotel-app

---
+++ app/Models/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'nama',
+        'username',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    
+}
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ isset($title) ? $title.' | '.config('app.name') : config('app.name') }}</title>
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+    <link rel="stylesheet" href="/adminlte/plugins/fontawesome-free/css/all.min.css">
+
+    <link rel="stylesheet" href="/adminlte/dist/css/adminlte.min.css?v=3.2.0">
+    <script nonce="ac978edf-43b4-4be9-8853-19adde033b09">
+        (function(w, d) {
+            ! function(a, e, t, r) {
+                a.zarazData = a.zarazData || {};
+                a.zarazData.executed = [];
+                a.zaraz = {
+                    deferred: []
+                };
+                a.zaraz.q = [];
+                a.zaraz._f = function(e) {
+                    return function() {
+                        var t = Array.prototype.slice.call(arguments);
+                        a.zaraz.q.push({
+                            m: e,
+                            a: t
+                        })
+                    }
+                };
+                for (const e of ["track", "set", "ecommerce", "debug"]) a.zaraz[e] = a.zaraz._f(e);
+                a.zaraz.init = () => {
+                    var t = e.getElementsByTagName(r)[0],
+                        z = e.createElement(r),
+                        n = e.getElementsByTagName("title")[0];
+                    n && (a.zarazData.t = e.getElementsByTagName("title")[0].text);
+                    a.zarazData.x = Math.random();
+                    a.zarazData.w = a.screen.width;
+                    a.zarazData.h = a.screen.height;
+                    a.zarazData.j = a.innerHeight;
+                    a.zarazData.e = a.innerWidth;
+                    a.zarazData.l = a.location.href;
+                    a.zarazData.r = e.referrer;
+                    a.zarazData.k = a.screen.colorDepth;
+                    a.zarazData.n = e.characterSet;
+                    a.zarazData.o = (new Date).getTimezoneOffset();
+                    a.zarazData.q = [];
+                    for (; a.zaraz.q.length;) {
+                        const e = a.zaraz.q.shift();
+                        a.zarazData.q.push(e)
+                    }
+                    z.defer = !0;
+                    for (const e of [localStorage, sessionStorage]) Object.keys(e || {}).filter((a => a.startsWith(
+                        "_zaraz_"))).forEach((t => {
+                        try {
+                            a.zarazData["z_" + t.slice(7)] = JSON.parse(e.getItem(t))
+                        } catch {
+                            a.zarazData["z_" + t.slice(7)] = e.getItem(t)
+                        }
+                    }));
+                    z.referrerPolicy = "origin";
+                    z.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(a.zarazData)));
+                    t.parentNode.insertBefore(z, t)
+                };
+                ["complete", "interactive"].includes(e.readyState) ? zaraz.init() : a.addEventListener(
+                    "DOMContentLoaded", zaraz.init)
+            }(w, d, 0, "script");
+        })(window, document);
+    </script>
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+
+        {{-- Navbar --}}
+        @include('layouts.inc_admin.navbar')
+        {{-- End Navbar --}}
+
+        {{-- Sidebar --}}
+        @include('layouts.inc_admin.sidebar')
+        {{-- End Sidebar --}}
+
+        <div class="content-wrapper">
+
+            <div class="content-header">
+                <div class="container-fluid mb-2">
+                    @yield('content-header')
+                </div>
+            </div>
+
+
+            <div class="content">
+                <div class="container-fluid">
+                    @yield('content')
+
+                </div>
+            </div>
+
+        </div>
+
+
+        <aside class="control-sidebar control-sidebar-dark">
+
+            <div class="p-3">
+                <h5>Title</h5>
+                <p>Sidebar content</p>
+            </div>
+        </aside>
+
+
+        <footer class="main-footer">
+
+            <div class="float-right d-none d-sm-inline">
+                Version 1.0.1
+            </div>
+
+            <strong>Copyright &copy; 2022 Rendi Xar.</strong> All rights
+            reserved.
+        </footer>
+    </div>
+
+
+
+    <script src="/adminlte/plugins/jquery/jquery.min.js"></script>
+
+    <script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="/adminlte/dist/js/adminlte.min.js?v=3.2.0"></script>
+</body>
+
+</html>
